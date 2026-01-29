7b42212ed247f5eac755b00d582b72d3ef36185a
NehaDhirendraSirur <sirurneha@gmail.com>
2026-01-30 00:39:49 +0530

    initialize Spring Boot backend module

---
+++ backend/src/main/java/com/neha/urlredirection/UrlredirectionApplication.java
@@ -0,0 +1,13 @@
+package com.neha.urlredirection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlredirectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlredirectionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/neha/urlredirection/UrlredirectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neha.urlredirection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlredirectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
